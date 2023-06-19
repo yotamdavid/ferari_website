10bc32e906e81d97a96a35ce7c225959c3f41865
yotamdavid <134198738+yotamdavid@users.noreply.github.com>
2023-06-19 17:28:05 +0300

    Update flaskrun.sh

--- web_project/flaskrun.sh
+++ web_project/flaskrun.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
-#downloading the updates
+
+# Check if Flask is already running on port 5000
+netstat -tln | grep ":5000 " > /dev/null
+if [[ $? -eq 0 ]]; then
+    echo "Flask is already running on port 5000. Exiting..."
+    exit 0
+fi
+
+# Install necessary packages
 sudo yum update -y
 sudo yum install git -y
 sudo yum install python3-pip -y
-cd  /home/ec2-user/ferari_website/web_project
+
+# Change to project directory
+cd /home/ec2-user/ferari_website/web_project
+
+# Install Flask
 sudo pip3 install flask
-FLASK_APP=app
+
+# Run Flask on port 5000
+export FLASK_APP=app
 flask run --host=0.0.0.0
