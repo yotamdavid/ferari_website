e4773713dcc3c544905f2677e8846198229a95c4
yotamdavid <134198738+yotamdavid@users.noreply.github.com>
2023-06-19 14:10:38 +0300

    Update flaskrun.sh

--- web_project/flaskrun.sh
+++ web_project/flaskrun.sh
@@ -3,6 +3,7 @@
 sudo yum update -y
 sudo yum install git -y
 sudo yum install python3-pip -y
-sudo python3 apt install flask -y
 cd  /home/ec2-user/ferari_website/web_project
+pip install -r requirements.txt
+FLASK_APP=app
 flask run --host=0.0.0.0
