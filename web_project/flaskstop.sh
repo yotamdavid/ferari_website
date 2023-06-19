459ac32ebf3efa9bac4bcfa405689119b61bb2a7
yotamdavid <134198738+yotamdavid@users.noreply.github.com>
2023-06-19 14:28:19 +0300

    Update flaskstop.sh

--- web_project/flaskstop.sh
+++ web_project/flaskstop.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Find the process IDs (PIDs) of the processes running on port 5000
-PIDS=$(sudo lsof -t -i :5000)
+PIDS=$(sudo lsof -t -i :8000)
 
 if [ -z "$PIDS" ]; then
-  echo "No processes found running on port 5000"
+  echo "No processes found running on port 8000"
 else
   for PID in $PIDS; do
     sudo kill -9 $PID
